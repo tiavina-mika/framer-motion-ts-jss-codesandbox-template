c4aed5fe1854d2e696435d65475ae941c52db1f7
TiavinaMichael <ralainirinatiavina@gmail.com>
2020-11-06 12:37:17 +0300

    change default animation

--- src/components/Animated.tsx
+++ src/components/Animated.tsx
@@ -2,22 +2,42 @@ import * as React from "react";
 import { motion } from "framer-motion";
 import { createUseStyles } from "react-jss";
 
+
 const useStyles = createUseStyles((theme: any) => ({
   animated: {
     composes: "flexRow justifyCenter alignCenter",
-    height: "100vh"
+    height: "100vh",
   },
-  motion: {
-    width: 200,
-    height: 200,
+  lineContainer: {
+    composes: "flexRow justifyCenter alignCenter flex1",
+    backgroundColor: 'green'
+  },
+  animatedLine: {
+    composes: "flexRow flex1",
+    height: 12,
     backgroundColor: theme.colors.primary
+  },
+  inactiveLine: {
+    composes: "stretchSelf",
+    backgroundColor: 'red'
   }
 }));
 const Animated = () => {
   const classes = useStyles();
   return (
     <div className={classes.animated}>
-      <motion.div animate={{ scale: 2 }} className={classes.motion} />
+      <div className={classes.lineContainer}>
+        <div className={classes.animatedLine}>
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: '100%' }} 
+            className={classes.inactiveLine}
+            transition={{
+              duration: 0.8,
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
